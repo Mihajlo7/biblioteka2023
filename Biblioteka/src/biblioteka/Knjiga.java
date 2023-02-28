f5ecbf22e1d5cda3a12829a8be8e8cd80e6ffba8
mihajlo7 <pav.mihajlo@gmail.com>
2023-02-28 10:22:58 +0100

    Dodati su atributi, geteri i seteri

--- Biblioteka/src/biblioteka/Knjiga.java
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -1,5 +1,40 @@
 package biblioteka;
 
 public class Knjiga {
+	private String autor;
+	private String izdavac;
+	private int izdanje;
+	private long isbn;
+	private String naslov;
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public String getIzdavac() {
+		return izdavac;
+	}
+	public void setIzdavac(String izdavac) {
+		this.izdavac = izdavac;
+	}
+	public int getIzdanje() {
+		return izdanje;
+	}
+	public void setIzdanje(int izdanje) {
+		this.izdanje = izdanje;
+	}
+	public long getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(long isbn) {
+		this.isbn = isbn;
+	}
+	public String getNaslov() {
+		return naslov;
+	}
+	public void setNaslov(String naslov) {
+		this.naslov = naslov;
+	}
 	
 }
