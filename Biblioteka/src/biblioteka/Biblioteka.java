a3e82c8c64e59580d4c4d3a2c793e29a5f06218e
mihajlo7 <pav.mihajlo@gmail.com>
2023-03-07 09:30:12 +0100

    Uvedena logicka kontrola na drugi nacin

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -29,6 +29,9 @@ public class Biblioteka implements BibliotekaInterfejs {
 
 	@Override
 	public List<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
+		if(autor==null && isbn<0 && naslov==null && izdavac==null) {
+			return null;
+		}
 		List<Knjiga> rezultati=new ArrayList<>();
 		for(Knjiga k:knjige) {
 			if(k.getNaslov().toLowerCase().contains(naslov.toLowerCase().trim())) {
